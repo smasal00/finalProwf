5741fdf1d64c1aaa0c26931a2cbe6b3ac934d148
Stefi Gandhi <smasal00@g.uafs.edu>
2020-08-04 20:12:18 -0500

    Updates

---
+++ precache-manifest.38bd0eb576d904bb3cb3ffa59acada58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0233c10eb8c8f173d762cf9aff03804",
+    "url": "/finalProwf/index.html"
+  },
+  {
+    "revision": "72f689ff1a8048100d9c",
+    "url": "/finalProwf/static/css/main.e6b65a01.chunk.css"
+  },
+  {
+    "revision": "205df78b6ec4e07689d8",
+    "url": "/finalProwf/static/js/2.0c0f3d95.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/finalProwf/static/js/2.0c0f3d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f689ff1a8048100d9c",
+    "url": "/finalProwf/static/js/main.10ac1bf2.chunk.js"
+  },
+  {
+    "revision": "773be8ed9774a395b4cf",
+    "url": "/finalProwf/static/js/runtime-main.a327c80e.js"
+  }
+]);
